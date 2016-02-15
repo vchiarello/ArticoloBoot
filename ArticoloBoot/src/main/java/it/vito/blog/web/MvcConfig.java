20e7aec5e6e346a566ba75381d967ff0f389c4f2
vchiarello <vito.chiarello@gmail.com>
2016-02-15 18:20:18 +0100

    Modifiche alla view di slideshow

--- ArticoloBoot/src/main/java/it/vito/blog/web/MvcConfig.java
+++ ArticoloBoot/src/main/java/it/vito/blog/web/MvcConfig.java
@@ -23,6 +23,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		registry.addViewController("/items/partialsEditSlideShowItem").setViewName("items/partialsEditSlideShowItem::content");
 		registry.addViewController("/items/login").setViewName("/items/login::content");
 		registry.addViewController("/login").setViewName("/items/login::content");
+		registry.addViewController("/TestSlideShow").setViewName("/items/TestSlideShow");
 		registry.addViewController("/templateAttendere").setViewName("/items/templateAttendere");
 	}
 	

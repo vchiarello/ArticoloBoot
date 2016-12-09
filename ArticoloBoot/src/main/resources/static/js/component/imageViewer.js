e0f0790ef3d4693e52ff464f3ca179f0cfa7bfcb
vito <vito.chiarello@gmail.com>
2016-12-09 17:11:14 +0100

    piccoli aggiustamenti e inizio carrello

--- ArticoloBoot/src/main/resources/static/js/component/imageViewer.js
+++ ArticoloBoot/src/main/resources/static/js/component/imageViewer.js
@@ -1,43 +1,39 @@
 function imageViewerCompCtrl(){
 	
-	  current=0;
+	current=0;
 	  
-	  this.so_xfade_prev=function(){
-		  if (current==0)current = this.immagini.length-1;
-		  else
-			  current--;
-		  attivaImmagine();
-	  }
-	  this.so_xfade_succ=function(){
-		  if (current==this.immagini.length-1)current = 0;
-		  else
-			  current++;
-		  attivaImmagine();
-	  }
+	this.so_xfade_prev=function(){
+		if (current==0)current = this.immagini.length-1;
+		else current--;
+		attivaImmagine();
+	}
 
-	  this.so_xfade_i = function(i){
-		  if (current==i)return;
-			current = i;
-			attivaImmagine();
-		}
+	this.so_xfade_succ=function(){
+		if (current==this.immagini.length-1)current = 0;
+		else current++;
+	  attivaImmagine();
+	}
 
-	  this.visibilitaCorrente = function (index){
-		  if (index==current)
-			  return "display:block"; 
-		  else
-			  return "display:none"; 
-		  
-		}  
+	this.so_xfade_i = function(i){
+		if (current==i)return;
+		current = i;
+		attivaImmagine();
+	}
+
+	this.visibilitaCorrente = function (index){
+		if (index==current) return "display:block"; 
+		else return "display:none"; 
+	}  
 	  
 
-	  this.calcolaImmagine = function(i){
-		  if (current==i)return "PallinoPieno.png";
-		  return "PallinoVuoto.png";
-	  }
+	this.calcolaImmagine = function(i){
+		if (current==i)return "PallinoPieno.png";
+		return "PallinoVuoto.png";
+	}
 
-	  this.displayImmagine = function(){
-		  attivaImmagine();
-	  }
+	this.displayImmagine = function(){
+		attivaImmagine();
+	}
 	  
 	function attivaImmagine(){
 		//valutare se mettere direttamente il display a none invece del fadeout
@@ -57,8 +53,7 @@ angular.module('blogApp').component('imageViewer', {
     templateUrl: 'html/imageViewer.html',
     controller: imageViewerCompCtrl,
     bindings:{
-    	immagini:'=',
-    	item:'='
+    	immagini:'='
     }
 });
 
